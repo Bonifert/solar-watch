08f767464f8c778343db5b548579e6bfe0a8abcb
Bonifert <116727997+Bonifert@users.noreply.github.com>
2023-10-25 16:21:18 +0200

    just to test the workflow

---
+++ src/test/java/com/codecool/solarwatch/service/SunriseSunsetServiceTest.java
@@ -0,0 +1,12 @@
+package com.codecool.solarwatch.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SunriseSunsetServiceTest {
+  @Test
+  void getSunriseAndSunset() {
+    assertEquals(1, 1);
+  }
+}
